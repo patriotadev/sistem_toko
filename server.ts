a036f346bfd52738a97de92a852ce123b0a25798
Patriota <patriota.alba12@gmail.com>
2023-09-20 15:21:33 +0700

    setup project

---
+++ server.ts
@@ -0,0 +1,19 @@
+import express, {Express, Request, Response} from 'express';
+import bodyParser from 'body-parser';
+import AppRouter from './src/router';
+import cors from 'cors';
+require('dotenv').config();
+const PORT: number = Number(process.env.PORT);
+const app: Express = express();
+
+app.use(cors());
+app.use(express.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use(AppRouter);
+
+app.listen(PORT, () => {
+    console.log('\x1b[36m',`Server is running on ${process.env.HOST_URL}:${process.env.PORT}`);
+}).on("error", () => {
+    console.log('\x1b[31m','Something wrong.. server error!')
+})
