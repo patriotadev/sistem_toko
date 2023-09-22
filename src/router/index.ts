7c7cebfdaae33430f23dea17101116549f5c35b1
Patriota <patriota.alba12@gmail.com>
2023-09-22 22:52:11 +0700

    [development - router] update index router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,15 @@
 import express, {Request, Response} from 'express';
+import StokRouter from '../modules/stok/stok.route';
+import TokoRouter from '../modules/toko/toko.route';
 
 const router = express.Router();
 
 router.get('/health', (req: Request, res: Response) => {
     res.send('App is healthy');
 });
+
+router.use('/api/stok', StokRouter);
+router.use('/api/toko', TokoRouter);
 
 router.use((req: Request, res: Response) => {
     res.status(400).send('Invalid route.');
